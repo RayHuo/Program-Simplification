c7006dbbaa4ad67d71f01e677e84ed499bc2d246
RayHuo <rayhuo_81@hotmail.com>
2014-12-08 01:10:06 +0800

    initial files added

---
+++ sources/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: ray
+ *
+ * Created on December 8, 2014, 12:47 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * This is the Program for both NLP and DLP simplification
+ */
+int main(int argc, char** argv) {
+    
+    
+    
+    return 0;
+}
+
